6bc5a93ede369a6d54c9d000b9a0ead4945249ec
Brandon Graybeal <bmgrayb@gmail.com>
2014-11-11 22:15:59 -0600

    Database complete. Small,test data inserted.

---
+++ dbFiles/testDataInsert.sql
@@ -0,0 +1,7 @@
+insert into linkedu.appuser values('test','test');
+insert into linkedu.APPOINTMENT values(100, 100, '2014-1-1');
+insert into linkedu.MULTIMEDIA values(default, 100, null, 4);
+insert into linkedu.RECRUITER values ('test', 'test', default, 100, 'test', 'test', 'test@test.com', false);
+insert into linkedu.STUDENT values ('test', 'test', default, null, null, null, null, null, null, null, null, null, null, null, null, null, true);
+insert into linkedu.UNIVERSITY values('test', 'test', default, 'Da Ilstu', null, null, null, null, null, null, true);
+
