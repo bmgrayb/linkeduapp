23a55a0678a526024d7bd433d0663ea08fc4765b
christopherObrien <c.obrien851@gmail.com>
2014-12-02 17:30:39 -0600

    Add apply model & controller, add sendEmail method to StuController

--- LinkedUApp/src/java/controller/StudentController.java
+++ LinkedUApp/src/java/controller/StudentController.java
@@ -17,6 +17,13 @@ import dao.RecruiterDAO;
 import dao.RecruiterDAOImpl;
 import dao.UniversityDAO;
 import dao.UniversityDAOImpl;
+import java.util.Properties;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import model.RecruiterModel;
 
 /**
@@ -96,7 +103,54 @@ public class StudentController {
         
         String subject = "I would like to know more about your university.";
         
-       // sendEmail(mailStr,stu.getEmail(),rec.getEmail(),subject);
+        sendEmail(mailStr,rec.getEmail(),stu.getEmail(),subject);
     }
+    
+    private void sendEmail(String text, String toEmail, String fromEmail, String subject){        
+         // Recipient's email ID needs to be mentioned.
+        String to = toEmail;
+        
+        // Sender's email ID needs to be mentioned
+        String from = fromEmail;
 
+        // Assuming you are sending email from this host
+        String host = "smtp.ilstu.edu";
+
+        // Get system properties
+        Properties properties = System.getProperties();
+
+        // Setup mail server
+        properties.setProperty("mail.smtp.host", host);
+        properties.setProperty("mail.user", "yourID"); // if needed
+        properties.setProperty("mail.password", "yourPassword"); // if needed
+
+        // Get the default Session object.
+        Session session = Session.getDefaultInstance(properties);
+
+        try {
+            // Create a default MimeMessage object.
+            MimeMessage message = new MimeMessage(session);
+
+            // Set From: header field of the header.
+            message.setFrom(new InternetAddress(from));
+
+            // Set To: header field of the header.
+            message.addRecipient(Message.RecipientType.TO,
+                    new InternetAddress(to));
+
+            // Set Subject: header field
+            message.setSubject(subject);
+
+            // Send the actual HTML message, as big as you like
+            message.setContent(text,
+                    "text/html");
+
+            // Send message
+            Transport.send(message);
+            System.out.println("Sent message successfully....");
+        } catch (MessagingException mex) {
+            mex.printStackTrace();
+        }
+        
+    }//end send mail
 }//end class
